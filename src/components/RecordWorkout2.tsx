bf86cfc5896e94cc9c94636a36f23a5b4b475cb1
Mandeep Singh <mandeeps708@gmail.com>
2023-01-30 00:38:11 +0530

    feat(session): store workout details in db
    - add toast for success/start
    - store workout details in the `history` indexed db collection
    - add some conditional rendering

--- src/components/RecordWorkout2.tsx
+++ src/components/RecordWorkout2.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useId, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 import { get, set } from '../data/ionicStorage';
-import { IonAccordion, IonAccordionGroup, IonButton, IonCol, IonInput, IonItem, IonLabel, IonRow, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonAccordion, IonAccordionGroup, IonButton, IonCol, IonInput, IonItem, IonLabel, IonRow, IonSelect, IonSelectOption, useIonToast } from '@ionic/react';
 import range from 'lodash.range';
 
 interface SessionProps {
@@ -9,19 +9,28 @@ interface SessionProps {
   setCurrentSession: React.Dispatch<React.SetStateAction<object>>
 };
 
+enum sessionStateEnum {
+  start,
+  wip,
+  finish
+}
+
 const RecordWorkout2: React.FC<SessionProps> = (props: SessionProps) => {
+  ;
+  const [toast] = useIonToast();
+
   console.log('props: ', props);
   const { currentSession, setCurrentSession } = props;
   const [workoutData, setWorkoutData] = useState([]);
   const [exerciseData, setExerciseData] = useState([]);
-  const [workoutRecordData, setWorkoutRecordData] = useState([]);
   const [selectedWorkout, setSelectedWorkout] = useState('');
   const [filteredExercises, setFilteredExercises] = useState([]);
+  const sessionId = Math.floor(10000 + Math.random() * 900000);
+  const [sessionState, setSessionState] = useState(sessionStateEnum.start);
 
   const fn = async () => {
     setWorkoutData(await get('workouts') || []);
     setExerciseData(await get('exercises') || []);
-    setWorkoutRecordData(await get('workoutRecords') || []);
   };
 
   const filterExercise = (workoutId: string) => {
@@ -53,94 +62,129 @@ const RecordWorkout2: React.FC<SessionProps> = (props: SessionProps) => {
     });
     console.log('setinitialdata: ', setsInitialData);
     setCurrentSession(setsInitialData);
+    setSessionState(sessionStateEnum.wip);
+    toast({
+      message: 'Starting Workout',
+      duration: 1500,
+      position: 'bottom'
+    });
   }
 
+  const finishWorkout = () => {
+    console.log('Finish workout: ', currentSession);
+    const data = {
+      id: sessionId,
+      workoutId: selectedWorkout,
+      data: currentSession,
+      createdAt: new Date()
+    }
+    set('history', data);
+    setSessionState(sessionStateEnum.finish);
+    toast({
+      message: 'Congratulations!',
+      duration: 2500,
+      position: 'bottom'
+    });
+  }
 
   useEffect(() => {
     fn();
   }, []);
-  console.log('all Data', workoutData, exerciseData, workoutRecordData);
+  // console.log('all Data', workoutData, exerciseData);
 
   return (
     <div>
-      <h1>Record Workout</h1>
+      {!(sessionState === sessionStateEnum.finish) ?
+        (
+          <div>
+            <h1>Record Workout</h1>
 
-      <IonSelect interface="action-sheet" placeholder="Select Workout"
-        onIonChange={(e) => {
-          setSelectedWorkout(e.detail.value);
-          const filteredEx = filterExercise(e.detail.value);
-          // if (e?.detail?.value && filteredEx.length) {
-          //   createSession(e.detail.value, filteredEx);
-          // }
-        }}
-      >
-        {workoutData.map((workout: any) => {
-          return (<div key={workout.id}>
-            <IonSelectOption value={workout.id}>{workout.name}</IonSelectOption>
-          </div>);
-        })}
-      </IonSelect>
+            <IonSelect interface="action-sheet" placeholder="Select Workout"
+              onIonChange={(e) => {
+                setSelectedWorkout(e.detail.value);
+                // const filteredEx = filterExercise(e.detail.value);
+                // if (e?.detail?.value && filteredEx.length) {
+                //   createSession(e.detail.value, filteredEx);
+                // }
+              }}
+            >
+              {workoutData.map((workout: any) => {
+                return (<div key={workout.id}>
+                  <IonSelectOption value={workout.id}>{workout.name}</IonSelectOption>
+                </div>);
+              })}
+            </IonSelect>
 
-      {
-        filteredExercises.map((exercise: any) => (
+            {filteredExercises.map((exercise: any) => (
+              <IonAccordionGroup>
+                <IonAccordion value="first">
+                  <IonItem slot="header" color="light">
+                    <IonLabel>{exercise.name}</IonLabel>
+                  </IonItem>
+                  <div className="ion-padding" slot="content">
+                    {/* First Content */}
+                    {
+                      [...Array(Number(exercise.sets))].map((_, index) => (
+                        <IonRow>
+                          <IonCol col-4>
+                            <IonLabel>Set {index + 1}</IonLabel>
+                          </IonCol>
+                          <IonCol col-4>
+                            <IonLabel>
+                              <IonInput
+                                // value={exerciseName}
+                                placeholder="Weight for the set"
+                                min="1"
+                                max="1000"
+                                type="number"
+                                inputmode="numeric"
+                                onIonBlur={e => updateCurrentSession(exercise.id, index, e.target.value!, null)}
+                                clearInput>
+                              </IonInput>
 
-          <IonAccordionGroup>
-            <IonAccordion value="first">
-              <IonItem slot="header" color="light">
-                <IonLabel>{exercise.name}</IonLabel>
-              </IonItem>
-              <div className="ion-padding" slot="content">
-                {/* First Content */}
-                {
-                  [...Array(Number(exercise.sets))].map((_, index) => (
-                    <IonRow>
-                      <IonCol col-4>
-                        <IonLabel>Set {index + 1}</IonLabel>
-                      </IonCol>
-                      <IonCol col-4>
-                        <IonLabel>
-                          <IonInput
-                            // value={exerciseName}
-                            placeholder="Weight for the set"
-                            min="1"
-                            max="1000"
-                            type="number"
-                            inputmode="numeric"
-                            onIonBlur={e => updateCurrentSession(exercise.id, index, e.target.value!, null)}
-                            clearInput>
-                          </IonInput>
+                            </IonLabel>
+                          </IonCol>
+                          <IonCol col-4>
+                            <IonLabel>
+                              <IonInput
+                                placeholder='Reps for the set'
+                                min='0'
+                                max='1000'
+                                type='number'
+                                inputmode='numeric'
+                                onIonBlur={e => updateCurrentSession(exercise.id, index, null, e.target.value!)}
+                                clearInput
+                              >
 
-                        </IonLabel>
-                      </IonCol>
-                      <IonCol col-4>
-                        <IonLabel>
-                          <IonInput
-                            placeholder='Reps for the set'
-                            min='0'
-                            max='1000'
-                            type='number'
-                            inputmode='numeric'
-                            onIonBlur={e => updateCurrentSession(exercise.id, index, null, e.target.value!)}
-                            clearInput
-                          >
+                              </IonInput>
+                              {/* {exercise.reps} */}
+                            </IonLabel>
+                          </IonCol>
+                        </IonRow>
+                      ))
+                    }
+                  </div>
+                </IonAccordion>
+              </IonAccordionGroup>
+            ))
+            }
 
-                          </IonInput>
-                          {/* {exercise.reps} */}
-                        </IonLabel>
-                      </IonCol>
-                    </IonRow>
-                  ))
-                }
-              </div>
-            </IonAccordion>
-          </IonAccordionGroup>
-        ))
-      }
-
-      {!!selectedWorkout &&
-        <IonButton onClick={setInitialSetData}>
-          Start
-        </IonButton>
+            {!!selectedWorkout && sessionState === sessionStateEnum.start &&
+              < IonButton onClick={setInitialSetData}>
+                Start
+              </IonButton>
+            }
+            {!([sessionStateEnum.finish, sessionStateEnum.start].includes(sessionState)) &&
+              <IonButton onClick={finishWorkout}>
+                Finish
+              </IonButton>
+            }
+          </div>
+        ) : (
+          <div>
+            <h1>Congratulations!</h1>
+          </div>
+        )
       }
     </div>
   );
